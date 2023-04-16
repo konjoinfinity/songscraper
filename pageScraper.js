8ae784ac3b87f58e8a63e3592a667bf91d2419f1
Wesley <wesleyscholl@gmail.com>
2023-04-16 18:10:19 -0400

    indexes were one value off, updated, testing difference between split formatting

--- pageScraper.js
+++ pageScraper.js
@@ -1,5 +1,4 @@
 const songJSON = require("./songTemplate.json");
-let songToSend = songJSON;
 const fs = require("fs").promises;
 const path = require("path");
 const process = require("process");
@@ -7,53 +6,32 @@ const { authenticate } = require("@google-cloud/local-auth");
 const { google } = require("googleapis");
 
 let documentCopyId;
-// If modifying these scopes, delete token.json.
+
 const SCOPES = [
   "https://www.googleapis.com/auth/drive.metadata.readonly",
   "https://www.googleapis.com/auth/documents",
   "https://www.googleapis.com/auth/drive",
   "https://www.googleapis.com/auth/drive.file",
 ];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
+
 const TOKEN_PATH = path.join(process.cwd(), "token.json");
 const CREDENTIALS_PATH = path.join(process.cwd(), "creds.json");
 
 const scraperObject = {
-  url: "https://tabs.ultimate-guitar.com/tab/bruno-mars/when-i-was-your-man-chords-1198871",
+  url: "https://tabs.ultimate-guitar.com/tab/acdc/you-shook-me-all-night-long-chords-621851",
   async scraper(browser) {
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
-    // Navigate to the selected page
     await page.goto(this.url);
-    // Wait for the required DOM to be rendered
     await page.waitForSelector(".P8ReX");
-
-    let capo = await page.$$eval(
-      "table > tr:nth-child(4) > td > span",
-      (options) => {
-        return options.map((option) => option.textContent);
-      }
-    );
-
-    //click -1
-    // await page.click("section > div:nth-child(7) > div > span.NWgb3 > button:nth-child(1)")
-
-    // capo = Number(capo.join('').charAt(0))
-    // console.log(capo)
-
-    // for(var i=0;i<capo;i++){
-    // 	// click +1
-    // 	await page.click("section > div:nth-child(7) > div > span.NWgb3 > button:nth-child(3)")
-    // }
-
-    console.log(capo);
 
     let first;
     let second;
     let third;
     let newTitle;
+    let middle;
+    let before;
+    let after;
     let string1;
     let string2;
 
@@ -61,7 +39,6 @@ const scraperObject = {
       first = await page.$$eval("pre > span", (options) => {
         return options.map((option) => option.textContent);
       });
-
       second = await page.$$eval("header > h1", (options) => {
         return options.map((option) => option.textContent);
       });
@@ -87,6 +64,48 @@ const scraperObject = {
       first = first.replaceAll("[Intro Tab]", "Intro Tab");
       first = first.replaceAll("[Instrumental]", "Instrumental");
       first = first.replaceAll("[Outro]", "Outro");
+
+      middle = Math.floor(first.length / 2);
+      before = first.lastIndexOf(" ", middle);
+      after = first.indexOf(" ", middle + 1);
+
+      if (middle - before < after - middle) {
+        middle = before;
+      } else {
+        middle = after;
+      }
+
+      string1 = first.substr(0, middle);
+      console.log(string1);
+      string2 = first.substr(middle + 1);
+
+// 	console.log(first.split(/\n/).length);
+// let chartArr = first.split(/\n/);
+// let stringy = chartArr.slice(0, 49);
+
+// let indexToSplit;
+// for (var i = 49; i > 34; i--) {
+//   if (stringy[i] === " ") {
+//     console.log("space");
+//     console.log(i);
+//     indexToSplit = i;
+//     break;
+//   } else {
+//     console.log("line");
+//     console.log(i);
+//   }
+// }
+
+// let colChart1 = chartArr.slice(0, indexToSplit);
+// console.log(colChart1);
+// let colChart2 = chartArr.slice(indexToSplit + 1, chartArr.length);
+// console.log(colChart2);
+
+      // Sort string1 and string2 into requests
+      // With bold and unbold based on regex patterns
+      // Insert starting at specified section title
+      // Read index location/range from last request
+      // Insert next request afer last request end
 
       /**
        * Reads previously authorized credentials from the save file.
@@ -149,38 +168,26 @@ const scraperObject = {
         const drive = google.drive({ version: "v3", auth: authClient });
         const docs = google.docs({ version: "v1", auth: authClient });
 
-        console.log(first.split(/\r\n|\r|\n/).length);
-        let chartArr = first.split(/\r\n|\r|\n/);
-        string1 = chartArr.slice(0, 49);
-
-        let indexToSplit;
-        for (var i = 49; i > 34; i--) {
-          if (string1[i] === " ") {
-            console.log("space");
-            console.log(i);
-            indexToSplit = i;
-            break;
-          } else {
-            console.log("line");
-            console.log(i);
-          }
-        }
-
-        let colChart1 = chartArr.slice(0, indexToSplit);
-        console.log(colChart1);
-        let colChart2 = chartArr.slice(indexToSplit + 1, chartArr.length);
-        console.log(colChart2);
-
         const titles =
           /(Chorus|Bridge|Outro|Intro|Verse|Verse 1|Verse 2|Verse 3|Instrumental|Interlude|Bridge|Intro Tab|Pre-chorus)/gi;
         const chords =
           /^[A-G][#b]?(m|maj|dim|aug|sus)?\d?(\/[A-G][#b]?)?(\s+[A-G][#b]?(m|maj|dim|aug|sus)?\d?(\/[A-G][#b]?)?)*$/;
+        var indexCount = 4;
+        const requests = [
+          {
+            replaceAllText: {
+              replaceText: newTitle,
+              containsText: {
+                text: "Song Title - Artist Name",
+                matchCase: true,
+              },
+            },
+          },
+        ];
 
-        const requests = [];
-        let indexCount = 4;
-
-        colChart1.forEach((line) => {
+        string1.split(/\n/).forEach((line, index) => {
           const isTitle = titles.test(line);
+          console.log(index);
           const isChord = chords.test(line.trim());
           if (!isTitle && !isChord) {
             requests.push({
@@ -227,78 +234,90 @@ const scraperObject = {
             });
             indexCount = indexCount + line.length;
           }
+		  console.log(indexCount)
         });
 
-        // 	  indexCount = indexCount + 2
-        // 	  string2.split("\n").forEach((line) => {
-        // 		const isTitle = titles.test(line);
-        // 		const isChord = chords.test(line.trim());
-        // 		if (!isTitle && !isChord) {
-        // 		  requests.push({
-        //   insertText: {
-        //     text: line,
-        //     location: {
-        //       index: indexCount + 1
-        //     }
-        //   }
-        // });
-        //  requests.push({
-        //   updateTextStyle: {
-        //     range: {
-        //       startIndex: indexCount + 1,
-        //       endIndex: indexCount + line.length
-        //     },
-        //     textStyle: {
-        //       bold: false
-        //     },
-        //     fields: "bold"
-        //   }
-        // 	});
-        //      indexCount = indexCount + line.length
-        // 		} else {
-        //       requests.push({
-        // 			insertText: {
-        //     text: line,
-        //     location: {
-        //       index: indexCount + 1
-        //     }
-        //   }
-        // 		  });
-        //      requests.push({
-        //   updateTextStyle: {
-        //     range: {
-        //       startIndex: indexCount + 1,
-        //       endIndex: indexCount + line.length
-        //     },
-        //     textStyle: {
-        //       bold: true
-        //     },
-        //     fields: "bold"
-        //   }
-        // 	});
-        //    indexCount = indexCount + line.length
-        //     }
-        // 	  });
-
-		console.log(requests)
-
-        await drive.files.copy(
-          {
-            fileId: "1xM26IwbTj7L9VNXwDLyXV4ZWSdLUvRybDclq_u46My4",
-            resource: { name: newTitle },
-          },
-          async (err, driveResponse) => {
-            documentCopyId = driveResponse.data.id;
-            console.log(driveResponse.data.id);
-            const updateResponse = await docs.documents.batchUpdate({
-              documentId: documentCopyId,
-              requestBody: {
-                requests: requests,
+		requests.push(  {
+            replaceAllText: {
+              replaceText: "2col2",
+              containsText: {
+                text: "col2",
+                matchCase: true,
               },
-            });
-            console.log(updateResponse.data);
-          }
-        );
+            },
+          })
+
+		//   string2.split("\n").forEach((line, index) => {
+		// 	const isTitle = titles.test(line);
+		// 	console.log(index);
+		// 	const isChord = chords.test(line.trim());
+		// 	if (!isTitle && !isChord) {
+		// 	  requests.push({
+		// 		insertText: {
+		// 		  text: line,
+		// 		  location: {
+		// 			index: indexCount + 1,
+		// 		  },
+		// 		},
+		// 	  });
+		// 	  requests.push({
+		// 		updateTextStyle: {
+		// 		  range: {
+		// 			startIndex: indexCount + 1,
+		// 			endIndex: indexCount + line.length,
+		// 		  },
+		// 		  textStyle: {
+		// 			bold: false,
+		// 		  },
+		// 		  fields: "bold",
+		// 		},
+		// 	  });
+		// 	  indexCount = indexCount + line.length;
+		// 	} else {
+		// 	  requests.push({
+		// 		insertText: {
+		// 		  text: line,
+		// 		  location: {
+		// 			index: indexCount + 1,
+		// 		  },
+		// 		},
+		// 	  });
+		// 	  requests.push({
+		// 		updateTextStyle: {
+		// 		  range: {
+		// 			startIndex: indexCount + 1,
+		// 			endIndex: indexCount + line.length,
+		// 		  },
+		// 		  textStyle: {
+		// 			bold: true,
+		// 		  },
+		// 		  fields: "bold",
+		// 		},
+		// 	  });
+		// 	  indexCount = indexCount + line.length;
+		// 	}
+		// 	console.log(indexCount)
+		//   });
+
+       
+		  console.log(JSON.stringify(requests))
+        // await drive.files.copy(
+        //   {
+        //     fileId: "1xM26IwbTj7L9VNXwDLyXV4ZWSdLUvRybDclq_u46My4",
+        //     resource: { name: newTitle },
+        //   },
+        //   async (err, driveResponse) => {
+        //     documentCopyId = driveResponse.data.id;
+        //     console.log(driveResponse.data.id);
+        //     const updateResponse = await docs.documents.batchUpdate({
+        //       documentId: documentCopyId,
+        //       requestBody: {
+        //         requests: requests,
+        //       },
+        //     });
+        //     console.log(updateResponse.data);
+        //   }
+        // );
       }
       authorize().then(listFiles).catch(console.error);
     }, 100);
